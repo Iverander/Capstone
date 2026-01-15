e88d645daaa6acbd8474d00f6baeaec2c043e84d
Iverander <torje@iverander.com>
2026-01-15 11:18:07 +0100

    update

--- Assets/!Scripts/DataCollection/Data.cs
+++ Assets/!Scripts/DataCollection/Data.cs
@@ -1,29 +1,52 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Firebase.Database;
 namespace Capstone
 {
     [Serializable]
-    public struct Data
+    public class Data
     {
         public string name;
         public string OS;
         public string CPU;
         public string GPU;
         public int RAM;
+        
+        public float averageFramerate;
+        //public List<float> framerate = new List<float>();
+
+        public void Initialize()
+        {
+            name = Environment.UserName;
+            OS = SystemInfo.operatingSystem;
+            CPU = SystemInfo.processorType;
+            GPU = SystemInfo.graphicsDeviceName;
+            RAM = SystemInfo.systemMemorySize;
+            
+            averageFramerate = -1f;
+            
+            Save();
+        }
 
         public override string ToString()
         {
-            return $"{OS} \n\n{CPU} \n\n{GPU} \n\n{RAM}GB";
+            return $"{OS} \n\n{CPU} \n\n{GPU} \n\n{RAM}";
+        }
+        
+        public void Save()
+        {
+            averageFramerate = Time.frameCount / Time.time;
+            
+            string json = JsonUtility.ToJson(this);
+
+            DataManager.database.Child("users").Child(name).SetRawJsonValueAsync(json);
         }
 
-        public Data(string name, string os ,string cpu, string gpu, int ram)
+        public void UpdateFramerate()
         {
-            this.name = name;
-            OS = os;
-            CPU = cpu;
-            GPU = gpu;
-            RAM = ram / 1000;
+            
+            //framerate.Add(1f / Time.deltaTime);
         }
     }
 }
--- Assets/!Scripts/DataCollection/DataManager.cs
+++ Assets/!Scripts/DataCollection/DataManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,31 +9,32 @@ namespace Capstone
 {
     public class DataManager : MonoBehaviour
     {
-        public static Data data;
-        public string usrID;
+        public static Data data = new Data();
         public UnityEvent<string> DataUpdated;
         
-        DatabaseReference database;
+        public static DatabaseReference database;
     
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Awake()
         {
             database = FirebaseDatabase.DefaultInstance.RootReference;
             
-            UpdateData();
+            data.Initialize();
+        }
+
+        private void OnApplicationQuit()
+        {
+            data.Save();
+        }
+
+        private void Update()
+        {
+            data.UpdateFramerate();
         }
 
         public void UpdateData()
         {
-            data = new Data(System.Environment.UserName, SystemInfo.operatingSystem, SystemInfo.processorModel, SystemInfo.graphicsDeviceName, SystemInfo.systemMemorySize);
             DataUpdated.Invoke(data.ToString());
-            Save();
-        }
-
-        public void Save()
-        {
-            string json = JsonUtility.ToJson(data);
-            database.Child("users").Child(data.name).SetRawJsonValueAsync(json);
         }
     }
 }
