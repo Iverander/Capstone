ad24cc021bc687d68f71e132f1d2c4792e387f2e
Iverander <torje@iverander.com>
2026-02-09 22:37:13 +0100

    push

--- Assets/!Scripts/Player/CombatAbility.cs
+++ Assets/!Scripts/Player/CombatAbility.cs
@@ -6,8 +6,12 @@ namespace Capstone
     [Serializable]
     public abstract class CombatAbility
     {
-        private Transform origin;
+        protected Transform origin;
         [field: SerializeField] public bool ShowGizmos { get; private set; }
+        
+        [Header("Stats")]
+        [SerializeField] protected Vector3 center = Vector3.up +Vector3.forward;
+        [SerializeField] protected Vector3 size = Vector3.one;
 
         public void Initialize(Transform origin)
         {
--- Assets/!Scripts/Player/Push.cs
+++ Assets/!Scripts/Player/Push.cs
@@ -6,9 +6,23 @@ namespace Capstone
     [Serializable]
     public class Push : CombatAbility
     {
+        [SerializeField] private float force;
         public override void Preform()
         {
             Debug.Log("Preform Push");
+            
+            Collider[] toPush = Physics.OverlapBox(center + origin.position, size / 2);
+            
+            if(toPush.Length <= 0) return;
+
+            foreach (Collider col in toPush)
+            {
+                Rigidbody rb = col.attachedRigidbody;
+                if(rb == null) continue;
+                
+                Debug.Log("Pushing " + col.gameObject.name);
+                rb.AddForce((rb.transform.position - origin.position) * force, ForceMode.Force);
+            }
         }
 
         public override void Gizmos(Transform origin)
@@ -16,7 +30,7 @@ namespace Capstone
             base.Gizmos(origin);
             
             UnityEngine.Gizmos.color = Color.yellow;
-            UnityEngine.Gizmos.DrawCube(Vector3.zero, Vector3.one);
+            UnityEngine.Gizmos.DrawCube(center, size);
         }
     }
 }
