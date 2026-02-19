9f85403cd585f370fceb827883f5afdc06e4beea
Iverander <torje@iverander.com>
2026-02-19 10:01:19 +0100

    datamanger

--- Assets/!Scripts/DataCollection/DataManager.cs
+++ Assets/!Scripts/DataCollection/DataManager.cs
@@ -12,13 +12,18 @@ namespace Capstone
         public Data data = new Data();
         
         public static DatabaseReference database;
+        
+        public static DataManager instance;
     
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            instance = this;
             database = FirebaseDatabase.DefaultInstance.RootReference;
             
             data.Initialize();
+            
+            DontDestroyOnLoad(gameObject);
         }
 
         private void OnApplicationQuit()
