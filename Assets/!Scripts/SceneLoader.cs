f2a338e5add1ebbdd64ceb32e82d91725b466971
Iverander <torje@iverander.com>
2026-03-17 23:09:24 +0100

    something i dont want pushed yet

--- Assets/!Scripts/SceneLoader.cs
+++ Assets/!Scripts/SceneLoader.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using AYellowpaper.SerializedCollections;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,9 +9,46 @@ namespace Capstone
 {
     public class SceneLoader : MonoBehaviour
     {
+        [Serializable]
+        public class Scene
+        {
+            [Scene] public string name;
+            public bool loaded;
+            public bool activateOnLoad;
+
+            public Scene(string name,  bool loaded = false, bool activateOnLoad = false)
+            {
+                this.name = name;
+                SceneManager.LoadScene(name, LoadSceneMode.Single);
+            }
+        }
+        
+        public static SceneLoader Instance { get; private set; }
+        [SerializedDictionary] public List<Scene> preLoadScenes = new();
+        public static Dictionary<string, Scene> LoadedScenes = new();
+
+        private void Start()
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            foreach (var scene in preLoadScenes)
+            {
+                LoadScene(scene.name);
+            }
+        }
+
+        public async void AsyncLoadScene(string scene)
+        {
+            LoadedScenes.Add(scene, new Scene(scene));
+        }
+        
         public static void LoadScene(string sceneName)
         {
-            SceneManager.LoadScene(sceneName);
+            if(LoadedScenes.ContainsKey(sceneName)) 
+                LoadedScenes[sceneName].loaded = true;
+            else
+                Instance.AsyncLoadScene(sceneName);
         }
     }
 }
